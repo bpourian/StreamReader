823ac9d2a4aff6d8d7270803b4c4a6fca9b43d44
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-08-25 16:03:08 +0100

    Read .txt file on console window
    Provide a string, file location path in the method and the app will
    display the .txt file in the console window.

---
+++ TXTFileReader/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace txtFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // .txt file reade code
+
+            Console.WriteLine("Insert the file path of .txt document to view:");
+                       
+            NotePadReader(Console.ReadLine());
+            
+            
+        }
+
+
+        /// <summary>
+        /// Insert file path in the form of a string and the 
+        /// method will return the content of the txt file
+        /// </summary>
+        /// <param name="filePath"></param>
+        private static void NotePadReader(string filePath)
+        {
+            StreamReader sr = null;
+
+            try
+            {                
+                sr = File.OpenText(filePath);
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.
+                }
+                                  
+            }
+        }
+    }
+}
